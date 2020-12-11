3ade04fe2d02ebad8ac26da19fdd04ea989443cf
Jinsei-Tsunda <66623654+Jinsei-Tsunda@users.noreply.github.com>
2020-12-12 01:32:03 +0800

    node server

---
+++ node/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+const router = require(__dirname + '/router')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+//router
+app.use('/router', router)
+
+//server listen
+app.listen(3000, function () {
+  console.log('server started, port 3000...')
+})
---
+++ node/src/router.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const router = express.Router()
+
+//read
+router.get('/', (req, res) => {})
+//create
+router.post('/add', (req, res) => {})
+//update
+router.put('/update', (req, res) => {})
+//delete
+router.delete('/delete', (req, res) => {})
+
+module.exports = router
