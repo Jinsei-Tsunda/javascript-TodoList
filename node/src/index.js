1205a90f7fe408f97a36126cf581eb0bfb11626c
Jinsei-Tsunda <66623654+Jinsei-Tsunda@users.noreply.github.com>
2020-12-16 22:45:51 +0800

    API

--- node/src/index.js
+++ node/src/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const router = require(__dirname + '/router')
+const router = require(__dirname + "/router");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 //router
-app.use('/router', router)
+app.use("/", router);
 
 //server listen
 app.listen(3000, function () {
-  console.log('server started, port 3000...')
-})
+  console.log("server started, port 3000...");
+});
--- node/src/router.js
+++ node/src/router.js
@@ -1,17 +1,43 @@
-const express = require('express')
-const router = express.Router()
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
-const db = require(__dirname + '/../db_connect')
+const db = require(__dirname + "/db_connect");
 
 //read
-router.get('/', (req, res) => {})
-//create
-router.post('/add', (req, res) => {})
-//update
-router.put('/update', (req, res) => {})
-//delete
-router.delete('/delete', (req, res) => {})
+// router.get("/api", async (req, res) => {
+//   const sql = "SELECT * FROM `todoList`";
+//   const [r] = await db.query(sql);
 
-module.exports = router
+//   res.json(r);
+// });
+
+//create
+router.post("/api/new", async (req, res) => {
+  const data = { ...req.body };
+  data.last_edited_date = new Date();
+  const sql = "INSERT INTO `todoList` SET ?";
+  const [r] = await db.query(sql, [data]);
+
+  res.json(r);
+});
+
+//update
+router.post("/api/edit/:sid", async (req, res) => {
+  const data = { ...req.body };
+  const sql = "UPDATE `address_book` SET ? WHERE `sid`=?";
+  const [r] = await db.query(sql, [data]);
+
+  res.json(r);
+});
+
+//delete
+router.delete("/api/del/:sid", async (req, res) => {
+  const sql = "DELETE FROM `todoList` WHERE sid=?";
+  const [r] = await db.query(sql, []);
+
+  res.json(r);
+});
+
+module.exports = router;
